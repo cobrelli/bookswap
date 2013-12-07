3328ff6fe4d81d5b1c79a7aa43af94531918f890
Tomi Virtanen <cobrelli@gmail.com>
2013-12-07 15:56:33 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookswap::Application.config.secret_key_base = 'e037f239a97a1eda2e52c8abca68796fd762bf6e373d1b6ff1af4fff82fc683f0859d5ab229b4b13b79230f1b386461d365b35ba9dac46804c332e9be08123e8'
