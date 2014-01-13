8be235f3a2e9f430b42674c142fdbf8495bbbe9c
Tomi Virtanen <cobrelli@gmail.com>
2014-01-13 21:58:17 +0200

    controller for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def index
+  	@users = User.all
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+      flash[:success] = "Lisäys onnistui!"
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :email)
+  end
 end
