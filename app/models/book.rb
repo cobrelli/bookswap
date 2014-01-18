714357f0e8eca6cb0e49114bce301698954784ff
Tomi Virtanen <cobrelli@gmail.com>
2014-01-18 09:39:05 +0200

    modelien siistiminen

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,8 @@
 class Book < ActiveRecord::Base
 
 	belongs_to :user
-	#has_one :user
-	#has_many :interests
-
+	
 	has_many :interests
-	#has_many :users, through: :interests
-
 
 	validates(:name, presence: true)
 	validates(:author, presence: true)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
 
 	has_many :books
-	
 	has_many :interests
-	#has_many :books, through: :interests
+
 	validates :name, presence: true
 	validates :email, presence: true
 end
