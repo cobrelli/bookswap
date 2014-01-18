b6bdfe8eae0cab3578e761194afb68b9e67cd74c
Tomi Virtanen <cobrelli@gmail.com>
2014-01-18 16:10:55 +0200

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+erkki = User.create name:"Erkki Esimerkki", email:"a@b.fi"
+maija = User.create name:"Maija Malli", email:"c@d.fi"
+
+a = Book.create name:"Kostoisku", value: 1, author:"Heikki Niljake"
+b = Book.create name:"Testikkeli", value: 1, author:"El Cono"
+c = Book.create name:"Golfkenttien guru", value: 1, author:"Matti Melkoinen"
+
+erkki.books << a
+erkki.books << b
+maija.books << c
