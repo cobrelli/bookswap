13fbdb628bed45492a08d0b6178262cdc376d205
Tomi Virtanen <cobrelli@gmail.com>
2014-01-19 12:50:29 +0200

    validations for name length and email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
 	has_many :books
 	has_many :interests
 
-	validates :name, presence: true
-	validates :email, presence: true
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
