bf7299c1c14af10386362f9b46c8f77ce1342c3f
Tomi Virtanen <cobrelli@gmail.com>
2014-03-22 11:28:19 +0200

    [navbar] lisätty user dropdown, logout placeholder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # määritellään, että metodi current_user tulee käyttöön myös näkymissä
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:user_id].nil? 
+    User.find(session[:user_id]) 
+  end
+
+  def ensure_that_signed_in 
+    redirect_to signin_path, notice:'you should be signed in' if current_user.nil?
+  end
+
+  def admin_user
+  	# puts "--------------"
+  	# puts current_user.admin?
+  	# puts current_user.id
+  	# puts "--------------"
+    redirect_to root_url unless current_user.admin?
+  end
 end
